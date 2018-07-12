6d6b8b109b0ca19feb3d862e971f94a826505656
DESKTOP-R5UV815\ASUS <1241339223@qq.com>
2018-07-12 10:14:10 +0800

    邻接图矩阵2

--- src/cn/edu/zucc/g4/service/CheckClassMap.java
+++ src/cn/edu/zucc/g4/service/CheckClassMap.java
@@ -3,25 +3,31 @@ package cn.edu.zucc.g4.service;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import javax.annotation.Resource;
+
 import cn.edu.zucc.g4.bean.CourseBean;
+import cn.edu.zucc.g4.bean.SelectionRecordBean;
 import cn.edu.zucc.g4.bean.StudentBean;
 import cn.edu.zucc.g4.dao.CourseDAO;
+import cn.edu.zucc.g4.dao.SelectionRecordDAO;
 import cn.edu.zucc.g4.dao.StudentDAO;
 
 public class CheckClassMap {
 
 	int[][] edges;//邻接表数组
-	
+	@Resource(name = "SelectionRecordDAO")
+	SelectionRecordDAO srdao;
 	ArrayList<CourseBean> courseList;////定义课程数组
 	CourseDAO csdao = new CourseDAO();
-	StudentDAO studao = new StudentDAO();
+	@Resource(name = "StudentDAO")
+	StudentDAO studao;
 	HashMap<String, Integer> csmap = new HashMap<String, Integer>();//定义课程对index的映射表
 	ArrayList<StudentBean> stuList = (ArrayList<StudentBean>) studao.listAllStudent();
-	
 	public CheckClassMap()
     {
+		stuList = (ArrayList<StudentBean>) studao.listAllStudent();
 		courseList= (ArrayList<CourseBean>) csdao.listALLCourse();
-		int mapsize = courseList.size();//定义图的大小为总课程数
+		int mapsize = courseList.size();//定义图的大小为总课程数// 
         edges = new int[mapsize][mapsize];
         for(int i=0;i<mapsize;i++) {//将默认值设为0
         	csmap.put(courseList.get(i).getCourse_id(), i);//设置课程id为主键,课程数组的下标为值
@@ -30,11 +36,11 @@ public class CheckClassMap {
         	}
         }
         for(int i=0;i<stuList.size();i++) {//遍历所有学生的课程
-        	ArrayList<CourseBean> templist = csdao.getClassidByStudentid(stuList.get(i).getStudent_id());//通过学生id查找课程
+        	ArrayList<SelectionRecordBean> templist = (ArrayList<SelectionRecordBean>) srdao.getCourseByStudentId(stuList.get(i).getStudent_id());//通过学生id查找课程
         	for(int j = 0;j<templist.size();j++) {//遍历此学生的所有课程
         		int nowindex;
         		nowindex =csmap.get(templist.get(j).getCourse_id());//获取课程下标
-        		for(int m= ;m<templist.size()-1;m++) {//遍历该学生此课程之后的课程并将其链接
+        		for(int m = 0;m<templist.size()-1;m++) {//遍历该学生此课程之后的课程并将其链接
         			edges[nowindex][m]=1;
         			edges[m][nowindex]=1;
         		}
@@ -49,6 +55,9 @@ public class CheckClassMap {
         }
         
     }
-	public check
-	
+	public int abs(String class1id,String class2id) {//通过两个classid检验
+		int id1= csmap.get(class1id);
+		int id2= csmap.get(class2id);
+		return edges[id1][id2];
+	}
 }
