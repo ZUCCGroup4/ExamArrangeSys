36d81c200fd28f2321bc5cf27653831434eb3b02
271898316@qq.com <271898316@qq.com>
2018-07-12 10:34:57 +0800

    hc change

---
+++ src/cn/edu/zucc/g4/bean/SelectionDetailBean.java
@@ -0,0 +1,26 @@
+package cn.edu.zucc.g4.bean;
+
+public class SelectionDetailBean {
+
+	private int selection_detail_id;
+	private String student_id;
+	private String selection_id;
+	public int getSelection_detail_id() {
+		return selection_detail_id;
+	}
+	public void setSelection_detail_id(int selection_detail_id) {
+		this.selection_detail_id = selection_detail_id;
+	}
+	public String getStudent_id() {
+		return student_id;
+	}
+	public void setStudent_id(String student_id) {
+		this.student_id = student_id;
+	}
+	public String getSelection_id() {
+		return selection_id;
+	}
+	public void setSelection_id(String selection_id) {
+		this.selection_id = selection_id;
+	}
+}
